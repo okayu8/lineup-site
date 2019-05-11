1b0686e681b0d6f2f14e25a38fffe0b9a69a85c1
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-05-11 11:56:28 +0900

    initialize repository

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+
+        //キャッシュが残るので 毎回 view ファイルを削除する
+        $success = \File::cleanDirectory(storage_path()."/framework/views/");
+
+        if(\Request::ip() == "61.27.15.240"){
+
+            //デバッグモード
+
+        } else {
+            \Debugbar::disable();
+        }
+
+
+    }
+
+}

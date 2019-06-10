d4f03f7351b67d02a61578c88b6616ef9cc5a6ce
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-06-11 00:41:53 +0900

    product create

--- resources/views/admin/product_create.blade.php
+++ resources/views/admin/product_create.blade.php
@@ -15,7 +15,7 @@
                     <div class="card-header">{{ __('Create') }}</div>
 
                     <div class="card-body">
-                        <form method="POST" action="/admin/products/store">
+                        <form method="POST" action="/admin/products">
                         {{ csrf_field() }}
                             <div class="form-group row">
                                 <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>

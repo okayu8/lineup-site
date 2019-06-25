d87dcfb187ba93fd73d54c83c52cc45a6183162d
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-06-25 15:51:53 +0900

    site setting controller作成中

---
+++ app/Services/SiteSettingService.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Services;
+use App\Setting;
+class SiteSettingService{
+
+    public function FirstSetting()
+    {
+        $setting = new Setting();
+        $setting->id = 0;
+        $setting->site_title = "My Site";
+        $setting->site_description = "";
+        $setting->title_image1 = "";
+        $setting->title_image2 = "";
+        $setting->title_image3 = "";
+        $setting->site_color = "";
+        $setting->save();
+
+        return;
+    }
+
+}

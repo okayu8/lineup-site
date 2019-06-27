2bac63b69aeed36c4ba513e756e9301c7c083ca6
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-06-27 19:41:05 +0900

    create profile service & controller

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -8,10 +8,40 @@ use App\Profile;
 use App\User;
 use DB;
 use Illuminate\Support\Facades\Auth;
-use App\Services\SiteSettingService;
+use App\Services\ProfileService;
 
 
 class ProfileController extends Controller
 {
-    //
+    protected $profileService;
+    public function __construct(ProfileService $profile_service)
+    {
+        $this->middleware('auth');
+        $this->profileService = $profile_service;
+    }
+
+    //profileを表示
+    public function index()
+    {
+        if(Profile::find(1)){
+            $profile = Profile::findOrFail(1);
+        }
+        else{
+            $this->profileService->FirstSetting();
+            $profile = Profile::find(1);
+        }
+        
+        return view('admin/profile_edit', ['profile' => $profile]);
+    }
+
+    //profileの更新
+    public function update(Request $request, $id)
+    {
+        $setting = Profile::findOrFail($id);
+        $setting->name = $request->name ? $request->name : "";
+        $setting->description = $request->description ? $request->description : "";
+        $setting->save();
+
+        return redirect('/admin');
+    }
 }

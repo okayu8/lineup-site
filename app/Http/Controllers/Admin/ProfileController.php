2848fcd16c58134c1fcd1a4fb8fd5240bfc16e7b
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-06-27 19:31:50 +0900

    create profile controller base

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Profile;
+use App\User;
+use DB;
+use Illuminate\Support\Facades\Auth;
+use App\Services\SiteSettingService;
+
+
+class ProfileController extends Controller
+{
+    //
+}

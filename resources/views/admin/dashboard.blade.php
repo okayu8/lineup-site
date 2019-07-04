535febcb2ea9719dd22f1dcd6055b36571927560
片岡祐太郎 <hitugaya12@gmail.com>
2019-07-04 11:43:38 +0900

    add icon in dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,16 +8,28 @@
 <h1 class="admin-title">DashBoard</h1>
 <div class="row dashboard-content">
     <div class="col-md-3 dashboard-item">
-        <a href="/admin/site-setting">Site Setting</a>
+        <a href="/admin/site-setting">
+            <img class="dashboard-icon" src="/images/icon/設定アイコン.svg">
+            <p class="dashboard-text">Site Setting</p>
+        </a>
     </div>
     <div class="col-md-3 dashboard-item">
-        <a href="/admin/products">Products</a>
+        <a href="/admin/products">
+            <img class="dashboard-icon" src="/images/icon/メニューの無料アイコン6.svg">
+            <p class="dashboard-text">Products</p>
+        </a>
     </div>
     <div class="col-md-3 dashboard-item">
-        <a href="/admin/profile">Profile</a>
+        <a href="/admin/profile">
+            <img class="dashboard-icon" src="/images/icon/個人情報ファイル1.svg">
+            <p class="dashboard-text">Profile</p>
+        </a>
     </div>
     <div class="col-md-3 dashboard-item">
-        <a href="admin/footer">Footer</a>
+        <a href="admin/footer">
+            <img class="dashboard-icon" src="/images/icon/リンクのフリーアイコン1.svg">
+            <p class="dashboard-text">Footer</p>
+        </a>
     </div>
 </div>
 @endsection

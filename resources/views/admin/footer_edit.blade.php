707827f3335918bcc4496fd091901a08c10b6d3e
片岡祐太郎 <hitugaya12@gmail.com>
2019-07-05 23:27:46 +0900

    fixed reguster button style in footer page

--- resources/views/admin/footer_edit.blade.php
+++ resources/views/admin/footer_edit.blade.php
@@ -43,7 +43,7 @@
                                     @endif
                                 </div>
                             </div>
-                            <div class="form-group mb-0">
+                            <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
                                         {{ __('Register') }}
